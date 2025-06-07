2e2cabb54630d2d59f69ce6689ae34a078ac3efb
faryd <mirandafaryd@gmail.com>
2025-06-07 12:17:56 -0500

    🔧 Refactor: function to get the base of the app
    the function was decoupled to obtain the app base and the global alias was added

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-
-const DEFAULT_BASE = '/';
-const [org, repo] = process.env.GITHUB_REPOSITORY?.split('/') || [];
-const base = process.env.VITE_BASE || (repo ? `/${repo}/` : DEFAULT_BASE);
+import { __appBase } from "./src/shared/utils/globals/app.config.js";
 
 export default defineConfig({
-  base: base,
+  base: __appBase,
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@shared': path.resolve(__dirname, './src/shared'),
+      '@globals': path.resolve(__dirname, './src/shared/utils/globals'),
     },
   },
 });
