95a66e023e7e834311dbd87d2a5b8ef77f7c6cbe
faryd <mirandafaryd@gmail.com>
2025-06-07 00:58:25 -0500

    🔧 Feat: dynamic base and alias configured
     logic for dynamic base deploy and alias added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+const DEFAULT_BASE = '/';
+const [org, repo] = process.env.GITHUB_REPOSITORY?.split('/') || [];
+const base = process.env.VITE_BASE || (repo ? `/${repo}/` : DEFAULT_BASE);
 
 export default defineConfig({
-  base: '/Frontend-Web-Application/',
+  base: base,
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 });
