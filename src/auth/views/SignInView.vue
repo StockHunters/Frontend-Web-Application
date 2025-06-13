0b216cd8dda1e00fb6bb34952c902ebdf1244fed
faryd <mirandafaryd@gmail.com>
2025-06-13 00:12:21 -0500

    👔 Feat: auth views added

--- src/auth/views/SignInView.vue
+++ src/auth/views/SignInView.vue
@@ -1,11 +1,42 @@
 <script>
 // Sign In || Login - Iniciar Sesión
+import TemplateAuth from "@/auth/components/TemplateAuth.vue";
+import GenericButton from "@shared/components/buttons/GenericButton.vue";
+import {tAuth} from "@shared/i18n/i18n.js";
+
 export default {
-  name: "SignInView"
+  name: "SignInView",
+  methods: {tAuth},
+  components: {GenericButton, TemplateAuth}
 }
 </script>
 
 <template>
+
+  <TemplateAuth aria-label="authentication form">
+    <template #form>
+      <h2>{{tAuth('login.title')}}</h2>
+
+      <form action="">
+        <input type="email"  :placeholder="$t('ui.form.placeholder.mail')" aria-label="email input" />
+        <input type="password" :placeholder="$t('ui.form.placeholder.password')" aria-label="password input"/>
+      </form>
+
+      <GenericButton link="/app"
+                     variant="color"
+                     aria-label="submit button"> {{$t('ui.form.submit')}}</GenericButton>
+
+      <GenericButton link=""
+                     variant="outline"
+                     icon="google"
+                     aria-label="google button"> {{$t('ui.button.google')}} </GenericButton>
+    </template>
+
+    <template #image>
+      <img src="@shared/assets/svgs/login.svg" width="400px" alt="login svg" aria-label="login image">
+    </template>
+  </TemplateAuth>
+
 </template>
 
 <style scoped>
--- src/auth/views/SignUpView.vue
+++ src/auth/views/SignUpView.vue
@@ -1,11 +1,44 @@
 <script>
 // Sign Up - Registrarse
+import TemplateAuth from "@/auth/components/TemplateAuth.vue";
+import {tAuth} from "@shared/i18n/i18n.js";
+import GenericButton from "@shared/components/buttons/GenericButton.vue";
+
 export default {
-  name: "SignUpView"
+  name: "SignUpView",
+  methods: {tAuth},
+  components: {GenericButton, TemplateAuth}
 }
 </script>
 
 <template>
+
+  <TemplateAuth aria-label="authentication form">
+
+    <template #form>
+      <h2>{{tAuth('signup.title')}}</h2>
+
+      <form action="" class="form">
+        <input type="text"  :placeholder="$t('ui.form.placeholder.name')" aria-label="name input"/>
+        <input type="text"  :placeholder="$t('ui.form.placeholder.lastname')" aria-label="lastname input"/>
+        <input type="email"  :placeholder="$t('ui.form.placeholder.mail')" aria-label="email input"/>
+        <input type="password" :placeholder="$t('ui.form.placeholder.password')" aria-label="password input"/>
+      </form>
+
+      <GenericButton link="/app"
+                     variant="color"
+                     aria-label="submit button"> {{$t('ui.form.submit')}}</GenericButton>
+
+      <GenericButton link=""
+                     variant="outline"
+                     icon="google"
+                     aria-label="access with google"> {{$t('ui.button.google')}} </GenericButton>
+    </template>
+
+    <template #image>
+      <img src="@shared/assets/svgs/login.svg" width="400px" alt="login svg" aria-label="login image">
+    </template>
+  </TemplateAuth>
 
 </template>
 
