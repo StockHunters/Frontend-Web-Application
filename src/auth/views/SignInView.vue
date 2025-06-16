961efd22ea5bc2c9f0a288d06dae6bfda1d9be64
faryd <mirandafaryd@gmail.com>
2025-06-16 13:09:30 -0500

    ⚡️ Improve: signin and signup view update
    logic for login and sign up created

--- src/auth/views/SignInView.vue
+++ src/auth/views/SignInView.vue
@@ -3,11 +3,37 @@
 import TemplateAuth from "@/auth/components/TemplateAuth.vue";
 import GenericButton from "@shared/components/buttons/GenericButton.vue";
 import {tAuth} from "@shared/i18n/i18n.js";
+import { SignInForm } from "@/auth/composables/useSignInForm.js";
+import SvgIcon from "@shared/lib/svg/SvgIcon.vue";
 
 export default {
   name: "SignInView",
-  methods: {tAuth},
-  components: {GenericButton, TemplateAuth}
+  components: {SvgIcon, GenericButton, TemplateAuth},
+  data(){
+    return {
+      email: "",
+      password: "",
+      erros: {}
+    }
+  },
+  methods: {
+    tAuth,
+
+    async handleSubmit(){
+      const form = new SignInForm({
+        email: this.email,
+        password: this.password,
+      });
+
+       const result = await form.login();
+
+      if (result && result.error) {
+        this.erros = result.error;
+      } else {
+        this.erros = {};
+      }
+    }
+  },
 }
 </script>
 
@@ -17,14 +43,32 @@ export default {
     <template #form>
       <h2>{{tAuth('login.title')}}</h2>
 
-      <form action="">
-        <input type="email"  :placeholder="$t('ui.form.placeholder.mail')" aria-label="email input" />
-        <input type="password" :placeholder="$t('ui.form.placeholder.password')" aria-label="password input"/>
-      </form>
+      <form @submit.prevent="handleSubmit">
+        <input type="email"
+               v-model="email"
+               :placeholder="$t('ui.form.placeholder.mail')"
+               aria-label="email input" />
 
-      <GenericButton link="/app"
-                     variant="color"
-                     aria-label="submit button"> {{$t('ui.form.submit')}}</GenericButton>
+        <p v-if="erros.email" class="error-message">
+          <SvgIcon name="warning"/> {{ erros.email }}</p>
+
+
+        <input type="password"
+               v-model="password"
+               :placeholder="$t('ui.form.placeholder.password')"
+               aria-label="password input"/>
+
+        <p v-if="erros.password" class="error-message">
+          <SvgIcon name="warning"/> {{erros.password }} </p>
+
+        <p v-if="erros.login" class="error-message">
+          <SvgIcon name="warning"/> {{ erros.login }}</p>
+
+        <GenericButton link=""
+                       type="submit"
+                       variant="color"
+                       aria-label="submit button"> {{$t('ui.form.submit')}}</GenericButton>
+      </form>
 
       <GenericButton link=""
                      variant="outline"
--- src/auth/views/SignUpView.vue
+++ src/auth/views/SignUpView.vue
@@ -3,11 +3,40 @@
 import TemplateAuth from "@/auth/components/TemplateAuth.vue";
 import {tAuth} from "@shared/i18n/i18n.js";
 import GenericButton from "@shared/components/buttons/GenericButton.vue";
+import {SignUpForm} from "@/auth/composables/useSignUpForm.js";
+import SvgIcon from "@shared/lib/svg/SvgIcon.vue";
 
 export default {
   name: "SignUpView",
-  methods: {tAuth},
-  components: {GenericButton, TemplateAuth}
+  components: {SvgIcon, GenericButton, TemplateAuth},
+  data(){
+    return {
+      email: "",
+      password: "",
+      firstname: "",
+      lastname: "",
+      errors: {}
+    }
+  },
+  methods: {tAuth,
+    async fetchData(){
+      const signUp = new SignUpForm({
+        firstname: this.firstname,
+        lastname: this.lastname,
+        email: this.email,
+        password: this.password
+      });
+
+      const result = await signUp.createUser();
+
+      if (result && result.errors) {
+        this.errors = result.errors;
+      } else {
+        this.errors = {};
+      }
+    }
+  }
+
 }
 </script>
 
@@ -18,16 +47,31 @@ export default {
     <template #form>
       <h2>{{tAuth('signup.title')}}</h2>
 
-      <form action="" class="form">
-        <input type="text"  :placeholder="$t('ui.form.placeholder.name')" aria-label="name input"/>
-        <input type="text"  :placeholder="$t('ui.form.placeholder.lastname')" aria-label="lastname input"/>
-        <input type="email"  :placeholder="$t('ui.form.placeholder.mail')" aria-label="email input"/>
-        <input type="password" :placeholder="$t('ui.form.placeholder.password')" aria-label="password input"/>
+      <form @submit.prevent="fetchData">
+        <input type="text" v-model="firstname" :placeholder="$t('ui.form.placeholder.name')" aria-label="name input"/>
+        <p v-if="errors.firstname" class="error-message">
+          <SvgIcon name="warning"/> {{ errors.firstname }}</p>
+
+        <input type="text" v-model="lastname"  :placeholder="$t('ui.form.placeholder.lastname')" aria-label="lastname input"/>
+        <p v-if="errors.lastname" class="error-message">
+          <SvgIcon name="warning"/> {{ errors.lastname }}</p>
+
+        <input type="email"  v-model="email" :placeholder="$t('ui.form.placeholder.mail')" aria-label="email input"/>
+        <p v-if="errors.email" class="error-message">
+          <SvgIcon name="warning"/> {{ errors.email }}</p>
+
+        <input type="password" v-model="password" :placeholder="$t('ui.form.placeholder.password')" aria-label="password input"/>
+        <p v-if="errors.password" class="error-message">
+          <SvgIcon name="warning"/> {{ errors.password }}</p>
+
+        <GenericButton link=""
+                       type="submit"
+                       variant="color"
+                       aria-label="submit button"> {{$t('ui.form.submit')}}</GenericButton>
+
       </form>
 
-      <GenericButton link="/app"
-                     variant="color"
-                     aria-label="submit button"> {{$t('ui.form.submit')}}</GenericButton>
+
 
       <GenericButton link=""
                      variant="outline"
