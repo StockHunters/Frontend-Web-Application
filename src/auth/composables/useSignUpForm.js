e20437c1d7f038c06f2ecfa2a82a0c2ce5d82166
faryd <mirandafaryd@gmail.com>
2025-06-16 13:33:30 -0500

    ✨ Feat: composables added
    login, signup and validation added

---
+++ src/auth/composables/useSignUpForm.js
@@ -0,0 +1,87 @@
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+import {FormValidation} from "@/auth/composables/useValidationForm.js";
+import i18n from '@shared/i18n/i18n.js';
+import {UserEntity} from "@/auth/models/user.entity.js";
+import {UserAssembler} from "@/auth/services/user.assembler.js";
+import {userService} from "@/auth/services/user.services.js";
+import {useAuth} from "@shared/composables/useAuth.js";
+import router from "@shared/router/index.js";
+const { t } = i18n.global;
+
+
+export class SignUpForm {
+    constructor({firstname, lastname, email, password}) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+
+        this.errors = {};
+    }
+
+    transformData() {
+        const orgList = ['ORG001','ORG002','ORG003','ORG004','ORG005'];
+        const roleList = [ 'admin' ,'user'];
+
+        const userId = `USR00${Math.floor(100 + Math.random() * 900)}`;
+        const randomOrg = orgList[Math.floor(Math.random() * orgList.length)];
+
+        const randomRole = roleList[Math.floor(Math.random() * roleList.length)];
+        const createdAt = new Date().toISOString();
+
+        return new UserEntity(
+            userId,
+            randomRole,
+            this.email.toLowerCase(),
+            this.email,
+            this.password,
+            this.firstname,
+            this.lastname,
+            randomOrg,
+            roleList[1],
+            createdAt
+        );
+    }
+
+    validateForm(){
+        const validated = new FormValidation(this.email, this.password);
+        this.errors = {};
+
+        const emailRequired = validated.requeried(this.email, t('alerts.email'));
+        const passwordRequired = validated.requeried(this.password, t('alerts.password'));
+        const firstnameRequired = validated.requeried(this.firstname, t('alerts.firstname'));
+        const lastnameRequired = validated.requeried(this.lastname, t('alerts.lastname'));
+
+        if (emailRequired) this.errors.email = emailRequired;
+        if (passwordRequired) this.errors.password = passwordRequired;
+        if (firstnameRequired) this.errors.firstname = firstnameRequired;
+        if (lastnameRequired) this.errors.lastname = lastnameRequired;
+
+        return Object.keys(this.errors).length === 0;
+    }
+
+    async createUser(){
+        this.errors = {};
+
+        if(!this.validateForm()){
+            return {success: false, errors: this.errors};
+        }
+
+        try{
+            const tempUser = this.transformData();
+            if(tempUser === null){
+                return {success: false, errors: this.errors};
+            }
+
+            await userService.create(UserAssembler.fromUser(tempUser));
+            await useAuth().login(tempUser.pwdHash, tempUser.id, tempUser.organizationId);
+            await router.push('/session');
+
+        } catch (error){
+            console.log(error);
+        }
+
+    }
+
+}
