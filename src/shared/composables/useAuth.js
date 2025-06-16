f75c23645b09f0888a356303822ea4a358163a74
faryd <mirandafaryd@gmail.com>
2025-06-16 13:39:32 -0500

    ⚡️ Improve: logout and login consts

--- src/shared/composables/useAuth.js
+++ src/shared/composables/useAuth.js
@@ -1,16 +1,22 @@
 import { computed } from 'vue'
+import router from "@shared/router/index.js";
 
 export function useAuth() {
     const getToken = () => localStorage.getItem('auth_token')
 
     const isAuthenticated = computed(() => !!getToken())
 
-    const login = (token) => {
+    const login = (token, id, org_id) => {
         localStorage.setItem('auth_token', token)
+        localStorage.setItem('id', id)
+        localStorage.setItem('org_id', org_id)
     }
 
     const logout = () => {
         localStorage.removeItem('auth_token')
+        localStorage.removeItem('id')
+        localStorage.removeItem('org_id')
+        router.push('/')
     }
 
     return {
