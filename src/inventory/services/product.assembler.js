bae4bc8c6f82ad6c571653c0c5e09b88358405cc
KenRi7 <enriquevaja16@gmail.com>
2025-07-05 23:58:56 -0500

    test4

--- src/inventory/services/product.assembler.js
+++ src/inventory/services/product.assembler.js
@@ -1,16 +1,10 @@
 import { Product } from "../models/product.entity.js";
 
 export class ProductAssembler {
-    /**
-     * Convierte un solo recurso plano (objeto JSON) en una entidad Product.
-     */
     static toEntityFromResource(resource = {}) {
         return new Product(resource);
     }
 
-    /**
-     * Convierte una respuesta API con estructura { data: [...] } en una lista de entidades Product.
-     */
     static toEntitiesFromResponse(response = {}) {
         if (!Array.isArray(response.data)) {
             console.warn("La respuesta de productos no es un array:", response);
